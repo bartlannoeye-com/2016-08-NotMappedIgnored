4e34c4834022573e31a9a5316245b7b9be0e07ed
bartlannoeye <bart.lannoeye@gmail.com>
2016-08-22 23:16:29 +0200

    Initial project checking, with error

---
+++ Src/NotMappedIgnored/NotMappedIgnored.Data/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotMappedIgnored.Data
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename=Configuration.db");
+        }
+    }
+}
---
+++ Src/NotMappedIgnored/NotMappedIgnored.Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotMappedIgnored.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [NotMapped]
+        public Guid[] Tokens { get; set; }
+    }
+}
