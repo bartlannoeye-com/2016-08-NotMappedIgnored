23349982b21272a2ed04a3e712f716bf47132bef
bartlannoeye <bart.lannoeye@gmail.com>
2016-08-22 23:21:12 +0200

    Applying the fix

---
+++ Src/NotMappedIgnored/NotMappedIgnored.Data/Migrations/20160822211835_FirstMigration.Designer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NotMappedIgnored.Data;
+
+namespace NotMappedIgnored.Data.Migrations
+{
+    [DbContext(typeof(MyContext))]
+    [Migration("20160822211835_FirstMigration")]
+    partial class FirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("NotMappedIgnored.Data.User", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Users");
+                });
+        }
+    }
+}
---
+++ Src/NotMappedIgnored/NotMappedIgnored.Data/Migrations/MyContextModelSnapshot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NotMappedIgnored.Data;
+
+namespace NotMappedIgnored.Data.Migrations
+{
+    [DbContext(typeof(MyContext))]
+    partial class MyContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("NotMappedIgnored.Data.User", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Users");
+                });
+        }
+    }
+}
